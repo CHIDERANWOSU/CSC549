47c525d9be67fa2ab5ce873ddc0c982f65998f92
Chidera O. Nwosu <33816842+ChideraNwosu@users.noreply.github.com>
2019-05-03 12:32:22 -0400

    Create Script.py

---
+++ NMR/Script.py
@@ -0,0 +1,3 @@
+from Bio.Align.Applications import ClustalwCommandline
+               cline = ClustalwCommandline("clustalw2", infile="/Users/dazhang/Downloads/clustalw-2.1-macosx/opuntia.fastahuman.fasta")
+               print(cline)
